deaab412eb0a82f78a37e6e1bf9303ccc50a8a55
Isaias Gonçalves <131933508+IsaiasZaza@users.noreply.github.com>
2025-06-02 16:35:32 -0300

    aaa

--- src/router.js
+++ src/router.js
@@ -175,6 +175,6 @@ router.post('/coupons/validate', async (req, res) => {
 });
 
 router.test('/test', (req, res) => {
-    res.status(200).json({ message: 'API is working!' });
+    res.status(200).json({ message: 'API is workinga!' });
 });
 module.exports = router;
